a724c4b6fa30a7a2ef00ec47d9ddec63a14da045
yuu2lee4 <yuu2lee4@qq.com>
2023-10-29 22:43:46 +0800

    openapi: 增加部分接口注释

--- routers/user.js
+++ routers/user.js
@@ -8,17 +8,29 @@ const auth = require('../middwares/auth')
  *   post:
  *     summary: "用户注册" 
  *     tags: [用户模块]
- *     parameters: 
- *       - name: name
- *         description: 账号
- *         required: true
- *         in: query
- *         type: string
- *       - name: password
- *         description: 密码
- *         in: query
- *         required: true
- *         type: string
+ *     requestBody: 
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - pin
+ *               - password
+ *               - repassword
+ *             properties:
+ *               name:
+ *                 description: 账号
+ *                 type: string
+ *               pin:
+ *                 description: 验证码
+ *                 type: string
+ *               password:
+ *                 description: 密码
+ *                 type: string
+ *               repassword:
+ *                 description: 重复密码
+ *                 type: string
  *     description: 用户注册
  *     produces: 
  *       - application/json
@@ -27,11 +39,75 @@ const auth = require('../middwares/auth')
  *         description: Returns a mysterious string.
  */
 router.post('/register', User.register)
+/**
+ * @openapi
+ * /user/resetPassword:
+ *   post:
+ *     summary: "重置密码" 
+ *     tags: [用户模块]
+ *     requestBody: 
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - pin
+ *               - password
+ *               - repassword
+ *             properties:
+ *               name:
+ *                 description: 账号
+ *                 type: string
+ *               pin:
+ *                 description: 验证码
+ *                 type: string
+ *               password:
+ *                 description: 密码
+ *                 type: string
+ *               repassword:
+ *                 description: 重复密码
+ *                 type: string
+ *     description: 密码重置
+ *     produces: 
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.post('/resetPassword', User.resetPassword)
 router.post('/login', User.login)
 router.post('/ldapLogin', User.ldapLogin)
 router.post('/borrow', auth.isLogin, User.borrow)
 router.post('/return', auth.isLogin, User.return)
+/**
+ * @openapi
+ * /user/getPin:
+ *   post:
+ *     summary: "获取验证码" 
+ *     tags: [用户模块]
+ *     requestBody: 
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - checkUser
+ *             properties:
+ *               name:
+ *                 description: 账号
+ *                 type: string
+ *               checkUser:
+ *                 description: 是否校验用户(注册为false或找回密码为true)
+ *                 type: boolean
+ *     description: 获取验证码
+ *     produces: 
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.post('/getPin', User.getPin)
 router.get('/', auth.isLogin, User.getUser)
 router.get('/logout', User.logout)
