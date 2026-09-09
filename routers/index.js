ff345c8b6977bdc32932de991e82c2f652a425c8
yul4 <yul4@knownsec.com>
2026-09-09 17:46:57 +0800

    增加微信读书接口代理

--- routers/index.js
+++ routers/index.js
@@ -2,10 +2,12 @@ import koaRouter from "koa-router";
 import book from "./book.js";
 import tag from "./tag.js";
 import user from "./user.js";
+import weread from "./weread.js";
 
 const api = koaRouter();
 api.use(book.routes());
 api.use(tag.routes());
 api.use(user.routes());
+api.use(weread.routes());
 
 export default api.use('/api', api.routes(), api.allowedMethods());
---
+++ routers/weread.js
@@ -0,0 +1,34 @@
+import koaRouter from "koa-router";
+import config from "config";
+
+// https://github.com/Tencent/WeChatReading/blob/main/skills/SKILL.md
+
+const router = koaRouter({ prefix: '/weread' });
+
+router.post('/gateway', async (ctx) => {
+	const wereadConfig = config.get('weread');
+	const response = await fetch(wereadConfig.gateway, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${wereadConfig.apiKey}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			...ctx.request.body,
+			skill_version: wereadConfig.skillVersion
+		})
+	});
+
+	const responseText = await response.text();
+	let responseBody;
+	try {
+		responseBody = JSON.parse(responseText);
+	} catch {
+		responseBody = responseText;
+	}
+
+	ctx.status = response.status;
+	ctx.body = responseBody;
+});
+
+export default router;
